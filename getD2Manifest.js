988364943beae2fa0e35f0f4bde83a2302108d54
Rob Jones <delphi.actual@gmail.com>
2019-06-23 10:04:55 -0600

    set language for manifest via cli

--- getD2Manifest.js
+++ getD2Manifest.js
@@ -1,10 +1,27 @@
 #!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
 const optionalRequire = require('optional-require')(require);
 const fs = require('fs');
 const request = require('request');
 const mkdirp = require('mkdirp');
 let latest = optionalRequire('./latest.json') || '';
 let filename;
+const languages = [
+  'de',
+  'en',
+  'es',
+  'es-mx',
+  'fr',
+  'it',
+  'ja',
+  'ko',
+  'pl',
+  'pt-br',
+  'ru',
+  'zh-chs',
+  'zh-cht'
+];
+const lc = languages.includes(argv.lc) ? argv.lc : 'en'; // specify language to download by --lc {lc} on command line "en" is default
 
 function writeFile(obj, filename) {
   const content = JSON.stringify(obj, null, 2);
@@ -15,15 +32,15 @@ function writeFile(obj, filename) {
   });
 }
 
-function onManifestRequest(error, response, body) {
+function onManifestRequest(error, response, body, lc) {
   var parsedResponse = JSON.parse(body);
-  var currVersion = parsedResponse.Response && parsedResponse.Response.jsonWorldContentPaths.en;
+  var currVersion = parsedResponse.Response && parsedResponse.Response.jsonWorldContentPaths[lc];
   filename = currVersion.split('/');
   filename = filename[filename.length - 1];
   if (parsedResponse.Response && latest !== currVersion) {
     var manifest = fs.createWriteStream(filename);
     request
-      .get(`https://www.bungie.net${parsedResponse.Response.jsonWorldContentPaths.en}`)
+      .get(`https://www.bungie.net${parsedResponse.Response.jsonWorldContentPaths[lc]}`)
       .pipe(manifest)
       .on('close', storeManifest);
     writeFile(currVersion, './latest.json');
