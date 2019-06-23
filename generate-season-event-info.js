62b12ecedd8890d379277fd6ff637929d2c68ec4
Rob Jones <delphi.actual@gmail.com>
2019-06-23 15:00:47 -0600

    seasonBlacklist and eventBlacklist

--- generate-season-event-info.js
+++ generate-season-event-info.js
@@ -10,32 +10,32 @@ const newEvent = {};
 
 const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 
-let inventoryItem = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
+const inventoryItem = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
 
 Object.keys(inventoryItem).forEach(function(key) {
   const hash = inventoryItem[key].hash;
   const categoryHashes = inventoryItem[key].itemCategoryHashes || [];
   const categoryBlacklist = [18, 1784235469, 53, 16]; // Currencies, Bounties, Quests, Quest Steps
-
-  const blacklisted = categoryHashes.some((v) => categoryBlacklist.indexOf(v) !== -1);
+  const seasonBlacklisted = categoryHashes.some((v) => categoryBlacklist.indexOf(v) !== -1);
+  const eventBlacklisted = false; // TODO will require collectible information
 
   items[hash] = JSON.stringify(inventoryItem[key]);
 
-  if (!blacklisted) {
+  if (!seasonBlacklisted) {
     // Only add items not currently in db and not blacklisted
     newSeason[hash] = seasons[hash] || calculatedSeason;
-  }
-  if (blacklisted) {
+  } else {
     // delete any items that got through before blacklist or when new blacklist items are added
     delete newSeason[hash];
   }
-  if (events[hash]) {
+
+  if (events[hash] && !eventBlacklisted) {
     // Only add event info, if none currently exists!
     newEvent[hash] = events[hash];
+  } else {
+    delete newEvent[hash];
   }
 });
-
-//console.log(JSON.parse(items[2362471601]));
 
 writeFile(newEvent, 'output/events.json');
 writeFile(newSeason, 'output/seasons.json');
