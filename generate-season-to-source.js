bf6d9b7d3392c71cc7a33f4a787f3a79b6e64835
Rob Jones <delphi.actual@gmail.com>
2019-07-03 23:25:11 -0600

    output seasons.json

--- generate-season-to-source.js
+++ generate-season-to-source.js
@@ -10,6 +10,7 @@ const collectibles = mostRecentManifestLoaded.DestinyCollectibleDefinition;
 // init an array in seasonNumbers for each season
 const seasonNumbers = [...Array(calculatedSeason + 1).keys()].slice(1);
 const seasonToSource = {};
+const itemSource = {};
 seasonNumbers.forEach((num) => (seasonToSource[num] = []));
 
 // loop through collectibles
@@ -18,14 +19,14 @@ Object.values(inventoryItems).forEach(function(item) {
   const season = seasonsMaster[item.hash];
   if (sourceHash && season) {
     seasonToSource[season].push(sourceHash);
+    itemSource[item.hash] = sourceHash;
   }
 });
 
 // uniq each season's collectibles
 seasonNumbers.forEach((season) => (seasonToSource[season] = [...new Set(seasonToSource[season])]));
 
-// Now to verify there are no intersections if intersections remove source from seasonToSource
-// put these into notSeasonallyUnique.json so we do not process these sources or items again
+// Now to verify there are no intersections; if intersections remove source from seasonToSource
 
 // notSeasonallyUnique contains sourceHashes which correspond to items in more than 1 season
 let notSeasonallyUnique = [];
@@ -47,3 +48,29 @@ seasonNumbers.forEach((season) => {
 });
 
 writeFilePretty('seasonToSource.json', seasonToSource);
+
+const seasons = {};
+const categoryBlacklist = [
+  16, // Quest Steps
+  18, // Currencies
+  40, // Material
+  53, // Quests
+  58, // Clan Banner
+  1784235469, // Bounties
+  2005599723, // Prophecy Offerings
+  2150402250, // Gags
+  2250046497, // Prophecy Tablets
+  2253669532, // Treasure Maps
+  3109687656 // Dummies
+];
+
+Object.values(inventoryItems).forEach(function(item) {
+  const categoryHashes = item.itemCategoryHashes || [];
+  const seasonBlacklisted = categoryBlacklist.filter((hash) => categoryHashes.includes(hash))
+    .length;
+  if (!notSeasonallyUnique.includes(itemSource[item.hash]) && !seasonBlacklisted) {
+    seasons[item.hash] = seasonsMaster[item.hash];
+  }
+});
+
+writeFilePretty('seasons.json', seasons);
