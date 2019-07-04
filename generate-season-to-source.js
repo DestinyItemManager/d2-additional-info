563d0397e5a15bfa34c424dc5431b886c04ec12c
sundevour <31990469+sundevour@users.noreply.github.com>
2019-07-03 20:48:02 -0700

    random cleanup

--- generate-season-to-source.js
+++ generate-season-to-source.js
@@ -11,38 +11,34 @@ const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 const inventoryItems = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
 const collectibles = mostRecentManifestLoaded.DestinyCollectibleDefinition;
 
-const seasonToSource = { 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [] };
+const thisSeason = 7;
 
-Object.keys(inventoryItems).forEach(function(key) {
-  const sourceHash = inventoryItems[key].collectibleHash
-    ? collectibles[inventoryItems[key].collectibleHash].sourceHash
-    : null;
-  const season = seasonsMaster[inventoryItems[key].hash];
+// init an array in seasonNumbers for each season
+const seasonNumbers = [...Array(thisSeason + 1).keys()].slice(1);
+const seasonToSource = {};
+seasonNumbers.forEach((num) => (seasonToSource[num] = []));
+
+// loop through collectibles
+Object.values(inventoryItems).forEach(function(item) {
+  const sourceHash = item.collectibleHash ? collectibles[item.collectibleHash].sourceHash : null;
+  const season = seasonsMaster[item.hash];
   if (sourceHash && season) {
     seasonToSource[season].push(sourceHash);
   }
 });
 
-seasonToSource[1] = _.uniq(seasonToSource[1]);
-seasonToSource[2] = _.uniq(seasonToSource[2]);
-seasonToSource[3] = _.uniq(seasonToSource[3]);
-seasonToSource[4] = _.uniq(seasonToSource[4]);
-seasonToSource[5] = _.uniq(seasonToSource[5]);
-seasonToSource[6] = _.uniq(seasonToSource[6]);
-seasonToSource[7] = _.uniq(seasonToSource[7]);
+// uniq each season's collectibles
+seasonNumbers.forEach((num) => (seasonToSource[num] = _.uniq(seasonToSource[num])));
 
 // Now to verify there are no intersections if intersections remove source from seasonToSource
 // put these into notSeasonallyUnique.json so we do not process these sources or items again
 
-intersections = [];
-for (var i = 1; i < 7; i++) {
-  for (var j = 1; j < 7; j++) {
-    if (i === j) {
-      continue;
-    }
-    intersections.push(_.intersection(seasonToSource[i], seasonToSource[j]));
-  }
-}
+const intersections = [];
+seasonNumbers.forEach((s1) => {
+  seasonNumbers.forEach((s2) => {
+    if (s1 !== s2) intersections.push(_.intersection(seasonToSource[s1], seasonToSource[s2]));
+  });
+});
 
 const notSeasonallyUnique = _.uniq(_.flatten(intersections));
 
