9c2d9a82c62ceccdaf2e72ca3310ac03c425b32c
sundevour <31990469+sundevour@users.noreply.github.com>
2019-07-27 11:04:54 -0700

    lkjnfrdpkjgnbeslkjrsjhbgskjtrbg

--- ci.sh
+++ ci.sh
@@ -10,5 +10,47 @@ echo -e "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.s
 git clone git@github.com:sundevour/d2-additional-info.git --depth 1
 cd d2-additional-info
 yarn install
+
+# fetch and process the english manifest
 bash generate-additional-info.sh
+echo status begin
 git status --porcelain
+echo status end
+
+# we're done if there are no changes
+if [ ! "$(git status --porcelain)" ]; then echo no change; exit 0; fi
+
+# abort if build caused a change besides the output folder
+if [ "$(git status --porcelain|grep -v output)" ]; then echo non-output change found; exit 1; fi
+
+
+# if we reached here, there's something to send to github
+cd ..
+git clone git@github.com:sundevour/d2-additional-info-module.git --depth 1
+cd d2-additional-info-module
+cp -f ../d2-additional-info/output/*.json ./
+
+# make a commit
+if [ "$MANIFEST_VERSION" ]
+    then commitmsg="update output - manifest v$MANIFEST_VERSION"
+        branchname="$MANIFEST_VERSION"
+    else commitmsg="update output - repo changed"
+        branchname="d2ai-config-update"
+fi
+
+git checkout -b $branchname
+git add -u
+git commit -m "$commitmsg"
+
+git status --porcelain|grep -e objective-triumph -e source-info -e inventoryitem-relationships
+
+# if there was a strings-generated change, create a PR
+if [ "$(git status --porcelain|grep -e objective-triumph -e source-info -e inventoryitem-relationships)" ]
+    then echo making a PR
+        #git push origin master
+        exit 0
+# changed files are dependable, push directly
+    else echo pushing
+        #git push origin branchname
+        exit 0
+fi
