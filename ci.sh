739437d99d56dc507f5199df12173ebcd0361a0d
sundevour <31990469+sundevour@users.noreply.github.com>
2019-07-27 06:00:29 -0700

    now with content

--- ci.sh
+++ ci.sh
@@ -1,2 +1,14 @@
-#!/bin/sh -exu
-echo $test
+git config --global user.email "31990469+sundevour@users.noreply.github.com"
+git config --global user.name "sundevour"
+
+# Decrypt SSH key
+openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in id_rsa.enc -out ~/.ssh/github.rsa -d
+chmod 600 ~/.ssh/github.rsa
+echo -e "Host github.com\n\tHostName github.com\n\tUser git\n\tIdentityFile ~/.ssh/github.rsa\n" >> ~/.ssh/config
+
+# Clone project
+git clone git@github.com:sundevour/d2-additional-info.git --depth 1
+cd d2-additional-info
+yarn install
+bash generate-additional-info.sh
+git status --porcelain
