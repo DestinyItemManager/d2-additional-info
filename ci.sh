b9c52442b4004d9f644a9166ff4c3e648bd5808a
sundevour <31990469+sundevour@users.noreply.github.com>
2019-07-28 11:41:42 -0700

    enable PR

--- ci.sh
+++ ci.sh
@@ -13,44 +13,62 @@ yarn install
 
 # fetch and process the english manifest
 bash generate-additional-info.sh
-echo status begin
-git status --porcelain
-echo status end
 
 # we're done if there are no changes
-if [ ! "$(git status --porcelain)" ]; then echo no change; exit 0; fi
+if [ ! "$(git status --porcelain)" ]
+    then echo no change
+    exit 0
+fi
 
-# abort if build caused a change besides the output folder
-if [ "$(git status --porcelain|grep -v output)" ]; then echo non-output change found; exit 1; fi
-
-
-# if we reached here, there's something to send to github
-cd ..
-git clone git@github.com:sundevour/d2-additional-info-module.git --depth 1
-cd d2-additional-info-module
-cp -f ../d2-additional-info/output/*.json ./
+# informational git status dump
+echo /=== begin changes after generate-additional-info.sh
+git status --porcelain
+echo end changes after generate-additional-info.sh ===/
 
 # make a commit
 if [ "$MANIFEST_VERSION" ]
-    then commitmsg="update output - manifest v$MANIFEST_VERSION"
+    then commitmsg="update all - manifest v$MANIFEST_VERSION"
+        dimcommitmsg="update configs - manifest v$MANIFEST_VERSION"
         branchname="$MANIFEST_VERSION"
-    else commitmsg="update output - repo changed"
-        branchname="d2ai-config-update"
+    else commitmsg="update all - repo changed - $TRAVIS_COMMIT"
+        dimcommitmsg="update configs - d2ai rules changed - $TRAVIS_COMMIT"
+        branchname="d2ai-config-update-$TRAVIS_COMMIT"
 fi
 
+# if we reached here, there's something to send to github
+# first we PR back to our own repo
 git checkout -b $branchname
 git add -u
 git commit -m "$commitmsg"
+git push origin $branchname
+exit 0
 
-git status --porcelain|grep -e objective-triumph -e source-info -e inventoryitem-relationships
+# then we clone DIM, and add our new files
+cd ..
+git clone git@github.com:sundevour/DIM --depth 1
+cd DIM
+cp -f ../d2-additional-info/output/*.json ./src/data/d2/
 
-# if there was a strings-generated change, create a PR
-if [ "$(git status --porcelain|grep -e objective-triumph -e source-info -e inventoryitem-relationships)" ]
-    then echo making a PR
-        git push origin $branchname
-        exit 0
-# changed files are dependable, push directly
-    else echo merging
-        git push origin master
-        exit 0
+echo /=== begin changes from existing d2-additional-info-module
+git status --porcelain
+echo end changes from existing d2-additional-info-module ===/
+
+# in case there are no changes, we're done
+if [ ! "$(git status --porcelain)" ]
+    then echo no change from existing DIM
+    exit 0
 fi
+
+# in case there are more changes than the intended folder, something went really wrong here
+if [ "$(git status --porcelain|grep -v src/data/d2)" ]
+    then echo non-config files changed in DIM
+    exit 1
+fi
+
+# create a DIM branch and make a PR
+echo making a PR
+git checkout -b $branchname
+git add -u
+git commit -m "$dimcommitmsg"
+git push origin $branchname
+exit 0
