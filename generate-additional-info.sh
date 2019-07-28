9ea31852c4d4936c822bd34131b2bb70e78f6ee3
Rob Jones <delphi.actual@gmail.com>
2019-07-28 08:54:57 -0600

    supplying any command line arg will skip downloading new manifest

--- generate-additional-info.sh
+++ generate-additional-info.sh
@@ -1,7 +1,18 @@
-node getD2Manifest &&
-node generate-season-event-info &&
-node generate-season-to-source &&
-node generate-source-info &&
-node generate-ghost-data &&
-node generate-objective-to-triumph &&
-yarn pretty
+ #!/bin/bash
+ if [ "$#" -eq  "0" ]
+    then
+        node getD2Manifest &&
+        node generate-season-event-info &&
+        node generate-season-to-source &&
+        node generate-source-info &&
+        node generate-ghost-data &&
+        node generate-objective-to-triumph &&
+        yarn pretty
+else
+    node generate-season-event-info &&
+    node generate-season-to-source &&
+    node generate-source-info &&
+    node generate-ghost-data &&
+    node generate-objective-to-triumph &&
+    yarn pretty
+fi
