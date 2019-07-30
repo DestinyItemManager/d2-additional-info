d14f10b21cf0535f787f49ac267b28e4fe2b9a6c
Rob Jones <delphi.actual@gmail.com>
2019-07-29 20:10:57 -0600

    ensure we have source data before generating events data

--- generate-additional-info.sh
+++ generate-additional-info.sh
@@ -3,18 +3,18 @@
     then
         node getD2Manifest &&
         node generate-season-info &&
-        node generate-event-info &&
         node generate-season-to-source &&
         node generate-source-info &&
+        node generate-event-info &&
         node generate-ghost-data &&
         node generate-objective-to-triumph &&
         node generate-bounty-data &&
         yarn pretty
 else
     node generate-season-info &&
-    node generate-event-info &&
     node generate-season-to-source &&
     node generate-source-info &&
+    node generate-event-info &&
     node generate-ghost-data &&
     node generate-objective-to-triumph &&
     node generate-bounty-data &&
