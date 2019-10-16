b42e0860b590fcd23c968d06347da5c2138fb656
Rob Jones <delphi.actual@gmail.com>
2019-10-15 22:15:43 -0600

    sort

--- generate-source-info.js
+++ generate-source-info.js
@@ -79,8 +79,14 @@ function categorizeSources() {
     D2Sources[alias] = D2Sources[source];
   });
 
+  // sort the object after adding in the aliases
+  D2SourcesSorted = {};
+  Object.keys(D2Sources)
+    .sort()
+    .forEach((k) => (D2SourcesSorted[k] = D2Sources[k]));
+
   let pretty = `const D2Sources: { [key: string]: { itemHashes: number[]; sourceHashes: number[] } } = ${stringifyObject(
-    D2Sources,
+    D2SourcesSorted,
     {
       indent: '  '
     }
