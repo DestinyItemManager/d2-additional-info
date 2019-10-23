3d9a272427df97c0bc97b388f256a7593e18ae1c
sundevour <31990469+sundevour@users.noreply.github.com>
2019-10-23 04:44:20 -0700

    seasonal mod slot identifiers

---
+++ generate-mod-slot-data.js
@@ -0,0 +1,23 @@
+const { writeFile, getMostRecentManifest } = require('./helpers.js');
+
+const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
+
+const inventoryItems = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
+
+const modTypeHashesByName = {};
+Object.values(inventoryItems).forEach((item) => {
+  if (
+    item.itemCategoryHashes &&
+    item.itemCategoryHashes.includes(59) &&
+    item.plug &&
+    (item.plug.plugCategoryIdentifier.includes('enhancements.season_') ||
+      item.plug.plugCategoryIdentifier.includes('enhancements.activity'))
+  ) {
+    const shortName = item.itemTypeDisplayName.toLowerCase().split(' ')[0];
+    if (!(shortName in modTypeHashesByName)) modTypeHashesByName[shortName] = [];
+    if (!modTypeHashesByName[shortName].includes(item.plug.plugCategoryHash))
+      modTypeHashesByName[shortName].push(item.plug.plugCategoryHash);
+  }
+});
+
+writeFile('./output/seasonal-mod-slots.json', modTypeHashesByName);
