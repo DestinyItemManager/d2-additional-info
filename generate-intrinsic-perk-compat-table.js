cc67248cc8b37153507b866ade00f02f61c0692d
Rob Jones <delphi.actual@gmail.com>
2019-11-01 17:26:07 -0600

    remove breaker types, rpm workaround for trace
    better lfr detection
    better fixup

--- generate-intrinsic-perk-compat-table.js
+++ generate-intrinsic-perk-compat-table.js
@@ -6,6 +6,16 @@ const inventoryItem = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
 
 const intrinsic = {};
 
+const itemCategoryHashBlackList = [
+  1, // Weapon
+  2, // Kinetic Weapon
+  3, // Energy Weapon
+  4, // Power Weapon
+  964228942, // Breaker: Disruption
+  1793728308, // Breaker: Piercing
+  2906646562 // Breaker: Stagger
+];
+
 Object.keys(inventoryItem).forEach(function(key) {
   const itemCategoryHashes = inventoryItem[key].itemCategoryHashes || [];
   if (
@@ -13,7 +23,9 @@ Object.keys(inventoryItem).forEach(function(key) {
     !itemCategoryHashes.includes(3109687656) && // not dummies
     inventoryItem[key].sockets
   ) {
-    const weaponType = itemCategoryHashes[itemCategoryHashes.length - 1]; // last in the array is the specific weapon type
+    const weaponTypeWide = ArrayDiff(itemCategoryHashes, itemCategoryHashBlackList);
+    const weaponType = NarrowWeaponType(weaponTypeWide);
+
     const rpm = getRPMorEQ(inventoryItem[key], weaponType);
     if (rpm > 0 || inventoryItem[key].inventory.tierType === 6) {
       // remove purples with weird stats
@@ -67,11 +79,65 @@ function getRPMorEQ(inventoryItem, weaponType) {
     case 54: //sword
       rpm = inventoryItem.stats.stats[2837207746].value; // swing speed
       break;
-    case 3317538576: // bows
+    case 3317538576: // bow
       rpm = inventoryItem.stats.stats[447667954].value; // draw time
       break;
     default:
       rpm = 0;
   }
+
+  // hash workaround for https://github.com/Bungie-net/api/issues/1131
+  const workAroundHashes = [
+    3524313097, // eriana's vow: 90rpm
+    1891561814, // whisper: 72rpm
+    2069224589, // 1kv: 1000ms
+    1852863732, // Wavesplitter: 1000rpm
+    4103414242 // Divinity: 1000rpm
+  ];
+
+  if (workAroundHashes.includes(inventoryItem.hash)) {
+    switch (inventoryItem.hash) {
+      case 3524313097: // Eriana's Vow
+        rpm = 90;
+        break;
+      case 1891561814: // Whisper
+        rpm = 72;
+        break;
+      case 1852863732: // Wavesplitter
+      case 2069224589: // 1KV
+      case 4103414242: // Divinity
+        rpm = 1000;
+        break;
+      default:
+        rpm = 0;
+    }
+  }
+
   return rpm;
+}
+
+function ArrayDiff(array1, array2) {
+  let array_difference = [];
+
+  // difference will contain duplicates
+  array_difference = array1.filter(function(x) {
+    if (array2.indexOf(x) == -1) return true;
+    else return false;
+  });
+
+  // create Set to eliminate duplicates
+  // convert Set to array using spread
+  return (filtered_array_difference = [...new Set(array_difference)]);
+}
+
+function NarrowWeaponType(array1) {
+  let weaponType;
+  if (array1.length > 1) {
+    if (array1.includes(1504945536)) {
+      weaponType = 1504945536;
+    }
+  } else {
+    weaponType = array1[0];
+  }
+  return weaponType;
 }
