3bccfd7da812d546d414f1d6c760cdf763cd10a3
Rob Jones <delphi.actual@gmail.com>
2019-11-02 11:11:44 -0600

    last cleanup

--- generate-intrinsic-perk-compat-table.js
+++ generate-intrinsic-perk-compat-table.js
@@ -27,7 +27,7 @@ Object.keys(inventoryItem).forEach(function(key) {
     const intrinsicPerkHash = inventoryItem[key].sockets.socketEntries[0].singleInitialItemHash;
     const isExotic = inventoryItem[key].inventory.tierType === 6;
     const weaponType = getWeaponType(itemCategoryHashes, inventoryItem[key].hash);
-    const rpm = getRPMorEQ(inventoryItem[key], weaponType);
+    const rpm = getRPM(inventoryItem[key], weaponType);
     if (rpm || isExotic) {
       // remove purples with weird stats
       if (isExotic) {
@@ -53,38 +53,30 @@ exoticIntrinsicList = uniqAndSortArray(exoticIntrinsicList);
 
 writeFile('./output/intrinsic-perk-lookup.json', intrinsic);
 
-function getRPMorEQ(inventoryItem, weaponType) {
+function getRPM(inventoryItem, weaponType) {
   // or equivalent per weaponType
-  let rpm;
   const stats = inventoryItem.stats.stats;
+  const hash = inventoryItem.hash;
 
-  const hashes = {
-    rpm: 4284893193,
-    chargeTime: 2961396640,
-    swingSpeed: 2837207746,
-    drawTime: 447667954
+  const weapons = {
+    rpm: {
+      hashes: [5, 6, 7, 8, 10, 11, 12, 13, 14, 153950757, 2489664120, 3954685534], // all other guns and launchers
+      stat: 4284893193 // rpm
+    },
+    charge: { hashes: [9, 1504945536], stat: 2961396640 }, // fusion & lfr; chargeTime
+    swing: { hashes: [54], stat: 2837207746 }, // sword; swingSpeed
+    draw: { hashes: [3317538576], stat: 447667954 } // bow; drawTime
   };
 
-  const weaponTypeStatLookup = {
-    5: stats[hashes.rpm], // auto rifle
-    6: stats[hashes.rpm], // hand cannon
-    7: stats[hashes.rpm], // pulse
-    8: stats[hashes.rpm], // scout
-    9: stats[hashes.chargeTime], // fusion rifle
-    10: stats[hashes.rpm], // sniper
-    11: stats[hashes.rpm], // shotgun
-    12: stats[hashes.rpm], // machine gun
-    13: stats[hashes.rpm], // rocket launcher
-    14: stats[hashes.rpm], // sidearm
-    54: stats[hashes.swingSpeed], // sword
-    153950757: stats[hashes.rpm], // grenade launcher
-    2489664120: stats[hashes.rpm], // trace rifle
-    3954685534: stats[hashes.rpm], // smg
-    1504945536: stats[hashes.chargeTime], // linear fusion rifle
-    3317538576: stats[hashes.drawTime] // bow
-  };
-
-  rpm = weaponTypeStatLookup[weaponType].value || 0;
+  const rpm = weapons.rpm.hashes.includes(weaponType)
+    ? stats[weapons.rpm.stat].value
+    : weapons.charge.hashes.includes(weaponType)
+    ? stats[weapons.charge.stat].value
+    : weapons.swing.hashes.includes(weaponType)
+    ? stats[weapons.swing.stat].value
+    : weapons.draw.hashes.includes(weaponType)
+    ? stats[weapons.draw.stat].value
+    : 0;
 
   // hash workaround for https://github.com/Bungie-net/api/issues/1131
   const workAroundHashes = {
@@ -95,16 +87,12 @@ function getRPMorEQ(inventoryItem, weaponType) {
     4103414242: 1000 // Divinity: 1000rpm
   };
 
-  if (workAroundHashes[inventoryItem.hash]) {
-    rpm = workAroundHashes[inventoryItem.hash];
-  }
-
-  return rpm;
+  return workAroundHashes[hash] ? workAroundHashes[hash] : rpm;
 }
 
 function getWeaponType(itemCategoryHashes, hash) {
   let weaponType;
-  const lfrHash = 1504945536;
+  const lfrHash = 1504945536; // lfr return as both lfr and fusion rifle
   itemCategoryHashes = diffArrays(itemCategoryHashes, itemCategoryHashExclusion);
 
   if (itemCategoryHashes.length > 1) {
