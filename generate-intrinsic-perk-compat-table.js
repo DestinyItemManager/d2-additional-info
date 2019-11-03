0e42e6a92cb0aaa22645c624b310bad2bbce6e4a
Rob Jones <delphi.actual@gmail.com>
2019-11-03 06:11:02 -0700

    use more constants and workaround by item name instead of hash remove redundant comments/re-org object

--- generate-intrinsic-perk-compat-table.js
+++ generate-intrinsic-perk-compat-table.js
@@ -1,8 +1,14 @@
 const { writeFile, getMostRecentManifest, uniqAndSortArray, diffArrays } = require('./helpers.js');
 const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 const inventoryItem = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
-const intrinsic = {};
-let exoticIntrinsicList = [];
+
+const WEAPON_CATEGORY_HASH = 1;
+const DUMMY_CATEGORY_HASH = 3109687656;
+const RPM_HASH = 4284893193;
+const DRAW_HASH = 447667954;
+const CHARGE_HASH = 2961396640;
+const SWING_HASH = 2837207746;
+const ONLY_EXOTICS = -99999999; // all other hashes are positive, so this is definitely ours
 
 const itemCategoryHashExclusion = [
   1, // Weapon
@@ -17,33 +23,66 @@ const itemCategoryHashExclusion = [
   2906646562 // Breaker: Stagger
 ];
 
+const weaponCategoryHashesToStat = {
+  5: RPM_HASH, // auto rifle
+  6: RPM_HASH, // hand cannon
+  7: RPM_HASH, // pulse rifle
+  8: RPM_HASH, // scout rifle
+  9: CHARGE_HASH, // fusion rifle
+  10: RPM_HASH, // sniper rifle
+  11: RPM_HASH, // shotgun
+  12: RPM_HASH, // machine gun
+  13: RPM_HASH, // rocket launcher
+  14: RPM_HASH, // sidearm
+  54: SWING_HASH, // sword
+  153950757: RPM_HASH, // grenade launcher
+  1504945536: CHARGE_HASH, // linear fusion rifle
+  2489664120: RPM_HASH, // trace rifle
+  3317538576: DRAW_HASH, // bow
+  3954685534: RPM_HASH // submachine gun
+};
+
+// workaround for https://github.com/Bungie-net/api/issues/1131
+const workAroundBadStats = {
+  Divinity: 1000,
+  "Eriana's Vow": 90,
+  'One Thousand Voices': 1000,
+  Wavesplitter: 1000,
+  'Whisper of the Worm': 72
+};
+
+const intrinsic = {};
+let exoticIntrinsicList = [];
+
 Object.keys(inventoryItem).forEach(function(key) {
   const itemCategoryHashes = inventoryItem[key].itemCategoryHashes || [];
   if (
-    itemCategoryHashes.includes(1) && // weapons only
-    !itemCategoryHashes.includes(3109687656) && // not dummies
+    itemCategoryHashes.includes(WEAPON_CATEGORY_HASH) &&
+    !itemCategoryHashes.includes(DUMMY_CATEGORY_HASH) &&
     inventoryItem[key].sockets
   ) {
     const intrinsicPerkHash = inventoryItem[key].sockets.socketEntries[0].singleInitialItemHash;
     const isExotic = inventoryItem[key].inventory.tierType === 6;
     const weaponType = getWeaponType(itemCategoryHashes, inventoryItem[key].hash);
-    const rpm = getRPM(inventoryItem[key], weaponType);
-    if (rpm || isExotic) {
+    const stat = workAroundBadStats[inventoryItem[key].displayProperties.name]
+      ? workAroundBadStats[inventoryItem[key].displayProperties.name]
+      : inventoryItem[key].stats.stats[weaponCategoryHashesToStat[weaponType]].value;
+    if (stat || isExotic) {
       // remove purples with weird stats
       if (isExotic) {
         // build a list of exotic intrinsic perks
         exoticIntrinsicList.push(intrinsicPerkHash);
       }
       if (intrinsic[weaponType]) {
-        intrinsic[weaponType][rpm]
-          ? intrinsic[weaponType][rpm].push(intrinsicPerkHash)
-          : (intrinsic[weaponType][rpm] = [intrinsicPerkHash]);
+        intrinsic[weaponType][stat]
+          ? intrinsic[weaponType][stat].push(intrinsicPerkHash)
+          : (intrinsic[weaponType][stat] = [intrinsicPerkHash]);
       } else {
         intrinsic[weaponType] = {};
-        intrinsic[weaponType][rpm] = [intrinsicPerkHash];
+        intrinsic[weaponType][stat] = [intrinsicPerkHash];
       }
-      intrinsic[weaponType][rpm] = uniqAndSortArray(intrinsic[weaponType][rpm]).sort(
-        (rpmHash) => !inventoryItem[rpmHash].displayProperties.name.includes('Frame')
+      intrinsic[weaponType][stat] = uniqAndSortArray(intrinsic[weaponType][stat]).sort(
+        (statHash) => !inventoryItem[statHash].displayProperties.name.includes('Frame')
       );
     }
   }
@@ -51,44 +90,16 @@ Object.keys(inventoryItem).forEach(function(key) {
 
 exoticIntrinsicList = uniqAndSortArray(exoticIntrinsicList);
 
+Object.values(intrinsic).forEach(function(weaponType) {
+  Object.values(weaponType).forEach(function(intrinsicList) {
+    const onlyExotics = diffArrays(intrinsicList, exoticIntrinsicList).length === 0;
+    if (onlyExotics) {
+      intrinsicList.splice(0, 0, ONLY_EXOTICS); // insert hash so we know this list only contains exotic perks
+    }
+  });
+});
+
 writeFile('./output/intrinsic-perk-lookup.json', intrinsic);
-
-function getRPM(inventoryItem, weaponType) {
-  // or equivalent per weaponType
-  const stats = inventoryItem.stats.stats;
-  const hash = inventoryItem.hash;
-
-  const weapons = {
-    rpm: {
-      hashes: [5, 6, 7, 8, 10, 11, 12, 13, 14, 153950757, 2489664120, 3954685534], // all other guns and launchers
-      stat: 4284893193 // rpm
-    },
-    charge: { hashes: [9, 1504945536], stat: 2961396640 }, // fusion & lfr; chargeTime
-    swing: { hashes: [54], stat: 2837207746 }, // sword; swingSpeed
-    draw: { hashes: [3317538576], stat: 447667954 } // bow; drawTime
-  };
-
-  const rpm = weapons.rpm.hashes.includes(weaponType)
-    ? stats[weapons.rpm.stat].value
-    : weapons.charge.hashes.includes(weaponType)
-    ? stats[weapons.charge.stat].value
-    : weapons.swing.hashes.includes(weaponType)
-    ? stats[weapons.swing.stat].value
-    : weapons.draw.hashes.includes(weaponType)
-    ? stats[weapons.draw.stat].value
-    : 0;
-
-  // hash workaround for https://github.com/Bungie-net/api/issues/1131
-  const workAroundHashes = {
-    1852863732: 1000, // Wavesplitter: 1000rpm
-    1891561814: 72, // Whisper: 72rpm
-    2069224589: 1000, // 1kv: 1000ms
-    3524313097: 90, // Eriana's Vow: 90rpm
-    4103414242: 1000 // Divinity: 1000rpm
-  };
-
-  return workAroundHashes[hash] ? workAroundHashes[hash] : rpm;
-}
 
 function getWeaponType(itemCategoryHashes, hash) {
   let weaponType;
