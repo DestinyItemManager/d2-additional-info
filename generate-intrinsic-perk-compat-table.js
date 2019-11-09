92ef558275904eda42bc7b797fd7dea2ab7d799c
Rob Jones <delphi.actual@gmail.com>
2019-11-08 18:03:55 -0700

    new intrinsic perk lookup table

--- generate-intrinsic-perk-compat-table.js
+++ generate-intrinsic-perk-compat-table.js
@@ -9,6 +9,7 @@ const DRAW_HASH = 447667954;
 const CHARGE_HASH = 2961396640;
 const SWING_HASH = 2837207746;
 const ONLY_EXOTICS = -99999999; // all other hashes are positive, so this is definitely ours
+const IMPACT_HASH = 4043523819;
 
 const itemCategoryHashExclusion = [
   1, // Weapon
@@ -43,7 +44,12 @@ const weaponCategoryHashesToStat = {
 };
 
 // workaround for https://github.com/Bungie-net/api/issues/1131
-const workAroundBadStats = {
+const workAroundBadImpact = {
+  Wavesplitter: 6,
+  Divinity: 6
+};
+
+const workAroundBadROF = {
   Divinity: 1000,
   "Eriana's Vow": 90,
   'Go Figure': 450,
@@ -53,7 +59,6 @@ const workAroundBadStats = {
 };
 
 const intrinsic = {};
-let exoticIntrinsicList = [];
 
 Object.keys(inventoryItem).forEach(function(key) {
   const itemCategoryHashes = inventoryItem[key].itemCategoryHashes || [];
@@ -63,44 +68,97 @@ Object.keys(inventoryItem).forEach(function(key) {
     inventoryItem[key].sockets
   ) {
     const intrinsicPerkHash = inventoryItem[key].sockets.socketEntries[0].singleInitialItemHash;
+    const frame = inventoryItem[intrinsicPerkHash].displayProperties.name;
     const isExotic = inventoryItem[key].inventory.tierType === 6;
     const weaponType = getWeaponType(itemCategoryHashes, inventoryItem[key].hash);
-    const stat = workAroundBadStats[inventoryItem[key].displayProperties.name]
-      ? workAroundBadStats[inventoryItem[key].displayProperties.name]
+
+    const impactOrRPM = workAroundBadImpact[inventoryItem[key].displayProperties.name]
+      ? workAroundBadImpact[inventoryItem[key].displayProperties.name]
+      : (inventoryItem[key].stats.stats[IMPACT_HASH] &&
+          inventoryItem[key].stats.stats[IMPACT_HASH].value) ||
+        (inventoryItem[key].stats.stats[RPM_HASH] &&
+          inventoryItem[key].stats.stats[RPM_HASH].value);
+
+    const rof = workAroundBadROF[inventoryItem[key].displayProperties.name]
+      ? workAroundBadROF[inventoryItem[key].displayProperties.name]
       : inventoryItem[key].stats.stats[weaponCategoryHashesToStat[weaponType]].value;
-    if (stat || isExotic) {
+
+    if (impactOrRPM || isExotic) {
       // remove purples with weird stats
-      if (isExotic) {
-        // build a list of exotic intrinsic perks
-        exoticIntrinsicList.push(intrinsicPerkHash);
-      }
-      if (intrinsic[weaponType]) {
-        intrinsic[weaponType][stat]
-          ? intrinsic[weaponType][stat].push(intrinsicPerkHash)
-          : (intrinsic[weaponType][stat] = [intrinsicPerkHash]);
+      if (intrinsic[weaponType] && intrinsic[weaponType][frame]) {
+        intrinsic[weaponType][frame].hashes
+          ? intrinsic[weaponType][frame].hashes.push(intrinsicPerkHash)
+          : (intrinsic[weaponType][frame].hashes = [intrinsicPerkHash]);
+        intrinsic[weaponType][frame].impact
+          ? intrinsic[weaponType][frame].impact.push(impactOrRPM)
+          : (intrinsic[weaponType][frame].impact = [impactOrRPM]);
+        intrinsic[weaponType][frame].rof
+          ? intrinsic[weaponType][frame].rof.push(rof)
+          : (intrinsic[weaponType][frame].rof = [rof]);
+        intrinsic[weaponType][frame].isExotic = isExotic;
+        intrinsic[weaponType][frame].isFrame =
+          !isExotic &&
+          !frame.includes('Omolon Adaptive Frame') &&
+          (frame.includes('Frame') || frame.includes('Aggressive Burst'));
       } else {
-        intrinsic[weaponType] = {};
-        intrinsic[weaponType][stat] = [intrinsicPerkHash];
+        intrinsic[weaponType] ? null : (intrinsic[weaponType] = {});
+        intrinsic[weaponType][frame] ? null : (intrinsic[weaponType][frame] = {});
+        intrinsic[weaponType][frame].hashes = [intrinsicPerkHash];
+        intrinsic[weaponType][frame].impact = [impactOrRPM];
+        intrinsic[weaponType][frame].rof = [rof];
+        intrinsic[weaponType][frame].isExotic = isExotic;
+        intrinsic[weaponType][frame].isFrame =
+          !isExotic &&
+          !frame.includes('Omolon Adaptive Frame') && // Omolon Adaptive Frame is compatible with Adaptive Frame
+          (frame.includes('Frame') || frame.includes('Aggressive Burst')); // Aggressive Burst is 4-shot pulse not compatible with any other pulse frames
       }
-      intrinsic[weaponType][stat] = uniqAndSortArray(intrinsic[weaponType][stat]).sort(
-        (statHash) => !inventoryItem[statHash].displayProperties.name.includes('Frame')
-      );
+      intrinsic[weaponType][frame].hashes = uniqAndSortArray(intrinsic[weaponType][frame].hashes);
+      intrinsic[weaponType][frame].impact = uniqAndSortArray(intrinsic[weaponType][frame].impact);
+      intrinsic[weaponType][frame].rof = uniqAndSortArray(intrinsic[weaponType][frame].rof);
     }
   }
 });
 
-exoticIntrinsicList = uniqAndSortArray(exoticIntrinsicList);
+compatTable = {
+  5: {}, // auto rifle
+  6: {}, // hand cannon
+  7: {}, // pulse rifle
+  8: {}, // scout rifle
+  9: {}, // fusion rifle
+  10: {}, // sniper rifle
+  11: {}, // shotgun
+  12: {}, // machine gun
+  13: {}, // rocket launcher
+  14: {}, // sidearm
+  54: {}, // sword
+  153950757: {}, // grenade launcher
+  1504945536: {}, // linear fusion rifle
+  2489664120: {}, // trace rifle
+  3317538576: {}, // bow
+  3954685534: {} // submachine gun
+};
 
-Object.values(intrinsic).forEach(function(weaponType) {
-  Object.values(weaponType).forEach(function(intrinsicList) {
-    const onlyExotics = diffArrays(intrinsicList, exoticIntrinsicList).length === 0;
-    if (onlyExotics) {
-      intrinsicList.splice(0, 0, ONLY_EXOTICS); // insert hash so we know this list only contains exotic perks
-    }
-  });
+// now search through intrinsic for isExotic = true and isFrame = false, and put them with compatible intrinsic perks based off of impact.
+// once all perks are grouped, if an intrinsic perk is by itself it is only compatible with itself, otherwise make an
+// array such that all element become a key containing an array of all compatible perks into compatTable.
+// e.g. 6: { ... "hashes": [895140517, 918679156, 1636108362] ... } becomes
+// 6: {
+//    ...
+//    895140517: [895140517, 918679156, 1636108362],
+//    918679156: [895140517, 918679156, 1636108362];
+//    1636108362: [895140517, 918679156, 1636108362];
+//    ...
+// },
+
+Object.entries(intrinsic).forEach(function(weaponType) {
+  console.log(weaponType);
+  // lost here
+  //Object.values(weaponType).forEach(function(frames) {
+  // console.log(frames);
+  //});
 });
 
-writeFile('./output/intrinsic-perk-lookup.json', intrinsic);
+writeFile('./output/intrinsic-perk-lookupV2.json', intrinsic);
 
 function getWeaponType(itemCategoryHashes, hash) {
   let weaponType;
