cc29cb92444669110bccf672075474762a8d83ad
sundevour <31990469+sundevour@users.noreply.github.com>
2020-03-14 12:18:15 -0700

    holdsmod support

--- generate-mod-slot-data.js
+++ generate-mod-slot-data.js
@@ -4,25 +4,90 @@ const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 
 const inventoryItems = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
 
-const modTypeHashesByName = {};
+// known mods specifically corresponding to seasons that i hope won't somehow change
+const seasonNumberByExampleMod = {
+  'Taken Armaments': 4,
+  'Fallen Armaments': 5,
+  'Hive Armaments': 7,
+  'Relay Defender': 8,
+  'Stacks on Stacks': 9,
+  'Blessing of Rasputin': 10
+};
+
+const modMetadataBySlotTag = {};
+const modTypeExampleHashesBySeason = {};
 
 Object.values(inventoryItems).forEach((item) => {
   if (
+    item.collectibleHash && // having a collectibleHash excludes the consumable mods
+    isSpecialtyMod(item) &&
+    item.displayProperties.name in seasonNumberByExampleMod // looking for only the specific mods listed above
+  ) {
+    const modSeason = seasonNumberByExampleMod[item.displayProperties.name];
+    if (!modTypeExampleHashesBySeason[modSeason])
+      modTypeExampleHashesBySeason[modSeason] = item.plug.plugCategoryHash;
+    // if (!modTypeExampleHashesBySeason[modSeason].includes(item.plug.plugCategoryHash)) {
+    //   modTypeExampleHashesBySeason[modSeason].push(item.plug.plugCategoryHash);
+    // }
+    // seasonsByModTypeHash[item.plug.plugCategoryHash] = modSeason;
+  }
+});
+
+Object.values(inventoryItems).forEach((item) => {
+  if (isSpecialtyMod(item)) {
+    const shortName = item.itemTypeDisplayName.toLowerCase().split(' ')[0];
+    if (!(shortName in modMetadataBySlotTag)) {
+      modMetadataBySlotTag[shortName] = {
+        thisSlotPlugCategoryHashes: [],
+        compatiblePlugCategoryHashes: []
+      };
+    }
+    if (
+      !modMetadataBySlotTag[shortName].thisSlotPlugCategoryHashes.includes(
+        item.plug.plugCategoryHash
+      )
+    ) {
+      modMetadataBySlotTag[shortName].thisSlotPlugCategoryHashes.push(item.plug.plugCategoryHash);
+    }
+    if (item.displayProperties.name === 'Empty Mod Socket') {
+      const matches = item.displayProperties.description.match(/\b\d+\b/g);
+      if (matches)
+        modMetadataBySlotTag[shortName].compatiblePlugCategoryHashes = matches.map((n) =>
+          Number(n)
+        );
+      // else console.log(item.hash, item.displayProperties.name,JSON.stringify(item.displayProperties.description))
+    }
+  }
+});
+for (tag in modMetadataBySlotTag) {
+  let allCompatibleSlotHashes = [];
+  modMetadataBySlotTag[tag].compatiblePlugCategoryHashes.forEach((seasonNumber) => {
+    const modMetadataForThisSeasonNumber = Object.values(modMetadataBySlotTag).find(
+      (singleModMetadata) =>
+        singleModMetadata.thisSlotPlugCategoryHashes.includes(
+          modTypeExampleHashesBySeason[seasonNumber]
+        )
+    );
+    const modTypesForThisSeasonNumber =
+      modMetadataForThisSeasonNumber && modMetadataForThisSeasonNumber.thisSlotPlugCategoryHashes;
+    if (modTypesForThisSeasonNumber)
+      allCompatibleSlotHashes = [...allCompatibleSlotHashes, ...modTypesForThisSeasonNumber];
+  });
+  modMetadataBySlotTag[tag].compatiblePlugCategoryHashes = allCompatibleSlotHashes;
+}
+const allPlugCategories = Object.values(modMetadataBySlotTag)
+  .map((modMetadata) => modMetadata.thisSlotPlugCategoryHashes)
+  .flat();
+writeFile('./output/specialty-modslot-metadata.json', modMetadataBySlotTag);
+writeFile('./output/specialty-modslot-plug-categories.json', allPlugCategories);
+
+function isSpecialtyMod(item) {
+  return (
     item.itemCategoryHashes &&
     item.itemCategoryHashes.includes(59) &&
     item.plug &&
     (item.plug.plugCategoryIdentifier.includes('enhancements.season_') ||
       item.plug.plugCategoryIdentifier.includes('enhancements.rivens_curse') ||
       item.plug.plugCategoryIdentifier.includes('enhancements.activity'))
-  ) {
-    const shortName = item.itemTypeDisplayName.toLowerCase().split(' ')[0];
-    if (!(shortName in modTypeHashesByName)) {
-      modTypeHashesByName[shortName] = [];
-    }
-    if (!modTypeHashesByName[shortName].includes(item.plug.plugCategoryHash)) {
-      modTypeHashesByName[shortName].push(item.plug.plugCategoryHash);
-    }
-  }
-});
-
-writeFile('./output/seasonal-mod-slots.json', modTypeHashesByName);
+  );
+}
