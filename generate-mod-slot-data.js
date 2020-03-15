490e58b0a6583ac6a622e1667434046644c1a9e1
sundevour <31990469+sundevour@users.noreply.github.com>
2020-03-14 19:47:33 -0700

    comments

--- generate-mod-slot-data.js
+++ generate-mod-slot-data.js
@@ -14,24 +14,48 @@ const seasonNumberByExampleMod = {
   'Blessing of Rasputin': 10
 };
 
+// about these hashes:
+// modslots themselves have no special slot affinity information.
+// mods expose a hash, corresponding to no perticular definition,
+// that determines where they can end up. so in DIM, we determine a modslot's type
+// by looking at the compatibility hash (plugCategoryHash) of the mod inside it.
+// this works on empty slots because "empty [season] socket" is also actually a mod.
+
+// this script generates a data structure that looks like:
+// {
+//   [key:string]:{
+//      thisSlotPlugCategoryHashes:number[],
+//      compatiblePlugCategoryHashes:number[]
+//   }
+// }
+//
+// i.e.:
+// {
+//   "forge": {
+//     "thisSlotPlugCategoryHashes": [65589297],
+//     "compatiblePlugCategoryHashes": [2149155760, 13646368, 65589297]
+//   },
+
+// anyway,
 const modMetadataBySlotTag = {};
 const modTypeExampleHashesBySeason = {};
 
+// since i don't want to assume itemHashes own't change, we look for some specific (y3) mods by name
 Object.values(inventoryItems).forEach((item) => {
   if (
-    item.collectibleHash && // having a collectibleHash excludes the consumable mods
+    item.collectibleHash && // having a collectibleHash excludes the consumable (y2) mods
     isSpecialtyMod(item) &&
     item.displayProperties.name in seasonNumberByExampleMod // looking for only the specific mods listed above
   ) {
     const modSeason = seasonNumberByExampleMod[item.displayProperties.name];
     if (!modTypeExampleHashesBySeason[modSeason])
       modTypeExampleHashesBySeason[modSeason] = item.plug.plugCategoryHash;
-    // if (!modTypeExampleHashesBySeason[modSeason].includes(item.plug.plugCategoryHash)) {
-    //   modTypeExampleHashesBySeason[modSeason].push(item.plug.plugCategoryHash);
-    // }
-    // seasonsByModTypeHash[item.plug.plugCategoryHash] = modSeason;
   }
 });
+
+// the point of the above was to get an example compatibility hash for each Season (4, 5, 7, 8, 9, etc)
+// this way we can identify the gathered compatibility hashes by season, to do the whole
+// "one season before, one season after" thing
 
 Object.values(inventoryItems).forEach((item) => {
   if (isSpecialtyMod(item)) {
@@ -49,6 +73,11 @@ Object.values(inventoryItems).forEach((item) => {
     ) {
       modMetadataBySlotTag[shortName].thisSlotPlugCategoryHashes.push(item.plug.plugCategoryHash);
     }
+
+    // we do special processing if one of the mods we looped through is an "empty slot" plug.
+    // from this mod's description, we can determine the "also compatible" seasons
+    // we need to finish collecting all hashes before we resolves seasons into groups of hashes,
+    // so for now, we stick season numbers instead of mod hashes, into compatiblePlugCategoryHashes
     if (item.displayProperties.name === 'Empty Mod Socket') {
       const matches = item.displayProperties.description.match(/\b\d+\b/g);
       if (matches)
@@ -59,6 +88,8 @@ Object.values(inventoryItems).forEach((item) => {
     }
   }
 });
+
+// we loop back through all the compatiblePlugCategoryHashes and turn their season #s into that season's compatibility hashes
 for (tag in modMetadataBySlotTag) {
   let allCompatibleSlotHashes = [];
   modMetadataBySlotTag[tag].compatiblePlugCategoryHashes.forEach((seasonNumber) => {
