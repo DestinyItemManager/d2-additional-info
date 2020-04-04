9618f19404e3193014edaa6654a5805c6228671f
Rob Jones <delphi.actual@gmail.com>
2020-04-04 09:39:33 -0600

    1st pass at catalyst source info

--- generate-catalyst-data.js
+++ generate-catalyst-data.js
@@ -1,4 +1,4 @@
-const { writeFile, getMostRecentManifest } = require('./helpers.js');
+const { writeFile, getMostRecentManifest, uniqAndSortArray } = require('./helpers.js');
 
 const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 
@@ -9,7 +9,7 @@ const record = mostRecentManifestLoaded.DestinyRecordDefinition;
 // this is keyed with record hashes, and the values are catalyst inventoryItem icons
 // (more interesting than the all-identical icons on catalyst triumphs)
 const triumphIcons = {};
-
+const sources = [];
 // loop the catalyst section of triumphs
 presentationNode[1111248994].children.presentationNodes.forEach((p) =>
   presentationNode[p.presentationNodeHash].children.records.forEach((r) => {
@@ -19,6 +19,19 @@ presentationNode[1111248994].children.presentationNodes.forEach((p) =>
         i.displayProperties.name === record[r.recordHash].displayProperties.name &&
         i.inventory.tierType === 6
     );
+    const item2WithSameName = Object.values(inventoryItem).find(
+      (i) =>
+        i.displayProperties.name === record[r.recordHash].displayProperties.name &&
+        i.itemType === 20 &&
+        i.displayProperties.iconSequences &&
+        i.displayProperties.iconSequences.length > 0
+    );
+    // and get its source
+    const source =
+      item2WithSameName &&
+      item2WithSameName.displayProperties &&
+      item2WithSameName.displayProperties.description;
+    sources.push(source);
     // and get its icon image
     const icon =
       itemWithSameName &&
@@ -29,5 +42,7 @@ presentationNode[1111248994].children.presentationNodes.forEach((p) =>
     else console.log(`${r.recordHash} ${record[r.recordHash].displayProperties.name}`);
   })
 );
+
+console.table(uniqAndSortArray(sources));
 
 writeFile('./output/catalyst-triumph-icons.json', triumphIcons);
