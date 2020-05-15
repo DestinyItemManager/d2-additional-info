9a6a7d6666f7056f06a37c066f9b3b70ae7547c9
Rob Jones <delphi.actual@gmail.com>
2020-05-14 18:49:27 -0600

    cleanup && isEqual helper

--- generate-missing-collectible-info.js
+++ generate-missing-collectible-info.js
@@ -1,6 +1,8 @@
-const { writeFile, getMostRecentManifest } = require('./helpers.js');
+const { writeFile, getMostRecentManifest, isEqual } = require('./helpers.js');
 const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 const inventoryItems = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
+
+missingCollectibleHashes = {};
 
 // Every Inventory Item without a collectible hash
 nonCollectibleItems = Object.values(inventoryItems).filter(function(item) {
@@ -16,22 +18,17 @@ collectibleItems = Object.values(inventoryItems).filter(function(item) {
   }
 });
 
-missingCollectibleHashes = {};
-
 Object.values(collectibleItems).forEach(function(item) {
-  // itemCategoryHashes may be missing on classified objects
-  collectibleHash = item.collectibleHash;
   itemsWithSameName = Object.values(nonCollectibleItems).filter(function(item2) {
     if (
       item.displayProperties.name === item2.displayProperties.name &&
-      JSON.stringify(item.itemCategoryHashes) === JSON.stringify(item2.itemCategoryHashes)
+      isEqual(item.itemCategoryHashes, item2.itemCategoryHashes)
     ) {
       return true;
     }
   });
-  // console.log(itemsWithSameName);
   Object.values(itemsWithSameName).forEach(function(item2) {
-    missingCollectibleHashes[item2.hash] = collectibleHash;
+    missingCollectibleHashes[item2.hash] = item.collectibleHash;
   });
 });
 
