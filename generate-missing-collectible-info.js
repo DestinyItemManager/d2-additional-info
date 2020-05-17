7c10e5c9dfdc8db1dedf4c6203e3120d5acbcc72
Rob Jones <delphi.actual@gmail.com>
2020-05-17 11:47:40 -0600

    getting there

--- generate-missing-collectible-info.js
+++ generate-missing-collectible-info.js
@@ -1,6 +1,9 @@
 const { writeFile, getMostRecentManifest, isEqual } = require('./helpers.js');
+const stringifyObject = require('stringify-object');
+
 const mostRecentManifestLoaded = require(`./${getMostRecentManifest()}`);
 const inventoryItems = mostRecentManifestLoaded.DestinyInventoryItemDefinition;
+const collectibles = mostRecentManifestLoaded.DestinyCollectibleDefinition;
 
 missingCollectibleHashes = {};
 
@@ -28,8 +31,119 @@ Object.values(collectibleItems).forEach(function(item) {
     }
   });
   Object.values(itemsWithSameName).forEach(function(item2) {
-    missingCollectibleHashes[item2.hash] = item.collectibleHash;
+    Object.values(collectibles).filter(function(collectible) {
+      if (item.collectibleHash === collectible.hash) {
+        missingCollectibleHashes[collectible.sourceHash]
+          ? missingCollectibleHashes[collectible.sourceHash].push(item2.hash)
+          : (missingCollectibleHashes[collectible.sourceHash] = [item2.hash]);
+      }
+    });
   });
 });
 
-writeFile('./output/missing-collectible-hashes.json', missingCollectibleHashes);
+// sourceHash: [itemHashes]
+writeFile('./data/sources/missing-collectible-hashes.json', missingCollectibleHashes);
+categorizeSources();
+
+function categorizeSources() {
+  const missingSources = missingCollectibleHashes;
+  let sourcesInfo = {};
+  let D2Sources = {}; // converts search field short source tags to item & source hashes
+
+  // sourcesInfo built from manifest collectibles
+  Object.values(collectibles).forEach(function(collectible) {
+    if (collectible.sourceHash) {
+      sourcesInfo[collectible.sourceHash] = collectible.sourceString;
+    }
+  });
+
+  // loop through categorization rules
+  /*Object.entries(categories.sources).forEach(function([sourceTag, matchRule]) {
+    // initialize this source's object
+    D2Sources[sourceTag] = {
+      itemHashes: [],
+      sourceHashes: []
+    };
+
+    // string match this category's source descriptions
+    D2Sources[sourceTag].sourceHashes = objectSearchValues(sourcesInfo, matchRule);
+    if (!D2Sources[sourceTag].sourceHashes.length) {
+      console.log(`no matching sources for: ${matchRule}`);
+    }
+
+    // add individual items if available for this category
+   /* if (categories.sources[sourceTag].items) {
+      categories.sources[sourceTag].items.forEach(function(itemNameOrHash) {
+        Object.entries(missingSources).forEach(function([itemHash, itemProperties]) {
+          if (
+            itemNameOrHash == itemHash ||
+            itemProperties.displayProperties.name == itemNameOrHash
+          ) {
+            D2Sources[sourceTag].itemHashes.push(itemHash);
+          }
+        });
+        D2Sources[sourceTag].itemHashes = uniqAndSortArray(D2Sources[sourceTag].itemHashes);
+      });
+    }
+
+
+    // lastly add aliases and copy info
+    if (categories.sources[sourceTag].alias) {
+      D2Sources[categories.sources[sourceTag].alias] = D2Sources[sourceTag];
+    }
+  });
+
+*/
+
+  // sort the object after adding in the aliases
+  D2SourcesSorted = {};
+  Object.keys(D2Sources)
+    .sort()
+    .forEach((k) => (D2SourcesSorted[k] = D2Sources[k]));
+
+  let pretty = `const missingSources: { [key: string]: number[] } = ${stringifyObject(
+    D2SourcesSorted,
+    {
+      indent: '  '
+    }
+  )};\n\nexport default missingSources;`;
+
+  // annotate the file with sources or item names next to matching hashes
+  let annotated = pretty.replace(/'(\d{2,})',?/g, function(match, submatch) {
+    if (sourcesInfo[submatch]) {
+      return `${Number(submatch)}, // ${sourcesInfo[submatch]}`;
+    }
+    if (inventoryItems[submatch]) {
+      return `${Number(submatch)}, // ${inventoryItems[submatch].displayProperties.name}`;
+    }
+    console.log(`unable to find information for hash ${submatch}`);
+    return `${Number(submatch)}, // could not identify hash`;
+  });
+
+  writeFile('./output/missing-source-info.ts', annotated);
+}
+
+// searches haystack (collected manifest source strings) to match against needleInfo (a categories.json match rule)
+// returns a list of source hashes
+function objectSearchValues(haystack, needleInfo) {
+  var searchResults = Object.entries(haystack); // [[hash, string],[hash, string],[hash, string]]
+
+  // filter down to only search results that match conditions
+  searchResults = searchResults.filter(
+    ([sourceHash, sourceString]) =>
+      // do inclusion strings match this sourceHash?
+      needleInfo.includes.filter((searchTerm) =>
+        sourceString.toLowerCase().includes(searchTerm.toLowerCase())
+      ).length &&
+      // not any excludes or not any exclude matches
+      !(
+        needleInfo.excludes &&
+        // do exclusion strings match this sourceHash?
+        needleInfo.excludes.filter((searchTerm) =>
+          sourceString.toLowerCase().includes(searchTerm.toLowerCase())
+        ).length
+      )
+  );
+  // extracts key 0 (sourcehash) from searchResults
+  return [...new Set(searchResults.map((result) => result[0]))];
+}
