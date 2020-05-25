13f9e26ea559dd1e269c4ed6e21b119fdd7f457a
Rob Jones <delphi.actual@gmail.com>
2020-05-24 19:47:55 -0600

    .

--- src/generate-catalyst-data.ts
+++ src/generate-catalyst-data.ts
@@ -7,7 +7,7 @@ const inventoryItems = getAll('DestinyInventoryItemDefinition');
 
 // this is keyed with record hashes, and the values are catalyst inventoryItem icons
 // (more interesting than the all-identical icons on catalyst triumphs)
-const triumphIcons: Record<string, string> = {};
+const triumphIcons: any = { icon: String, source: String };
 
 // loop the catalyst section of triumphs
 get('DestinyPresentationNodeDefinition', 1111248994)?.children.presentationNodes.forEach((p) =>
@@ -20,14 +20,68 @@ get('DestinyPresentationNodeDefinition', 1111248994)?.children.presentationNodes
         (i) => i.displayProperties.name === recordName && i.inventory.tierType === 6
       );
 
-      // and get its icon image
-      const icon = itemWithSameName?.displayProperties?.icon;
+      const item2WithSameName = inventoryItems.find(
+        (i) =>
+          i.displayProperties.name === recordName &&
+          i.itemType === 20 &&
+          i.displayProperties.iconSequences &&
+          i.displayProperties.iconSequences.length > 0
+      );
+
+      const name = itemWithSameName?.displayProperties?.name;
+      // and get its source
+      const source = `Source: ${
+        item2WithSameName?.displayProperties?.description
+          ? item2WithSameName?.displayProperties?.description
+          : NoSourceToSource(name)
+      }`;
 
       // this "if" check is because of classified data situations
-      if (icon) triumphIcons[r.recordHash] = icon;
-      else console.log(`no catalyst image found for ${r.recordHash} ${recordName}`);
+      if (source) {
+        triumphIcons[r.recordHash] = {};
+        triumphIcons[r.recordHash].name = name;
+        triumphIcons[r.recordHash].icon = itemWithSameName?.displayProperties?.icon;
+        triumphIcons[r.recordHash].source = source;
+      } else {
+        console.log(`no catalyst image found for ${r.recordHash} ${recordName}`);
+      }
     }
   )
 );
 
 writeFile('./output/catalyst-triumph-icons.json', triumphIcons);
+
+function NoSourceToSource(name: any) {
+  switch (name) {
+    case 'Symmetry Catalyst':
+      return 'Complete the "Symmetry Remastered" quest from Banshee-44.';
+    case 'Outbreak Perfected Catalyst':
+      return 'Found in the Heroic version of Zero Hour';
+    case 'Cerberus+1 Catalyst':
+      return 'Found by defeating the enemies of humanity wherever they lurk.';
+    case 'Bad Juju Catalyst':
+      return 'Found in The Tribute Hall';
+    case 'SUROS Regime Catalyst':
+      return 'Found by earning victory in the Crucible.';
+    case "Izanagi's Burden Catalyst":
+      return 'Found in the heroic version of the Menagerie';
+    case 'Ace of Spades Catalyst':
+      return 'Found in strikes against the most challenging opponents.';
+    case 'The Fourth Horseman Catalyst':
+      return 'Found in Seraph Tower Public Events or Legendary Lost Sectors';
+
+    case 'Lumina Catalyst':
+    case 'Black Talon Catalyst':
+    case 'Lord of Wolves Catalyst':
+      return 'Found in strikes and the Crucible.';
+
+    case "Eriana's Vow Catalyst":
+    case "Tommy's Matchbook Catalyst":
+      return 'Found in Season Pass';
+
+    case 'Bastion Catalyst':
+    case "Devil's Ruin Catalyst":
+    default:
+      return 'Not currently available.';
+  }
+}
