abc11d3083a56b3e5f148a617903f29e98682d56
Rob Jones <delphi.actual@gmail.com>
2020-06-13 18:30:17 -0600

    cleanup and async fix clarify

--- src/generate-watermark-to-season.ts
+++ src/generate-watermark-to-season.ts
@@ -23,44 +23,46 @@ const watermarks = [
 const watermarkToSeason = {} as Record<string, number>;
 const existingWatermarks = [] as string[];
 
-(async () => {
-  watermarks.forEach((uri) => {
-    const filename = uri.substring(uri.lastIndexOf('/') + 1);
-    downloadFile(`https://www.bungie.net${uri}`, `./data/seasons/watermarks/test/${filename}`);
-  });
+const watermarkDIR = `./data/seasons/watermarks/`;
+const watermarkTestDIR = `${watermarkDIR}test/`;
 
-  let newSeason;
-  for (let seasonX = 1; seasonX <= D2CalculatedSeason; seasonX++) {
+(async () => {
+  // Download all season watermarks from Bungie
+  for await (const uri of watermarks) {
+    await downloadFile(
+      `https://www.bungie.net${uri}`,
+      `${watermarkTestDIR}${uri.substring(uri.lastIndexOf('/') + 1)}`
+    );
+  }
+
+  // compare season watermarks from Bungie to previously downloaded images from Bungie
+  let newSeason = null;
+  for (let season = 1; season <= D2CalculatedSeason; season++) {
     let match = false;
-    for (let season = 1; season <= D2CalculatedSeason; season++) {
-      const testFileName = watermarks[season - 1].substring(
-        watermarks[season - 1].lastIndexOf('/') + 1
-      );
-      const testFile = `./data/seasons/watermarks/test/${testFileName}`;
-      const goodFile = `./data/seasons/watermarks/${seasonX}.png`;
-      const same = fse.existsSync(goodFile) && (await imagesSame(testFile, goodFile));
-      if (same) {
-        existingWatermarks.push(watermarks[season - 1]);
-        watermarkToSeason[watermarks[season - 1]] = seasonX;
+    for await (const uri of watermarks) {
+      const filenameFromBungie = uri.substring(uri.lastIndexOf('/') + 1);
+      const BungieFile = `${watermarkTestDIR}${filenameFromBungie}`;
+      const D2AIFile = `${watermarkDIR}${season}.png`;
+      if (fse.existsSync(D2AIFile) && (await imagesSame(D2AIFile, BungieFile))) {
+        existingWatermarks.push(uri); // used to detect new season overlay via filter later
+        watermarkToSeason[uri] = season;
         match = true;
         continue;
       }
     }
     if (!match) {
       // New Season overlay detected!
-      newSeason = seasonX;
+      newSeason = season;
     }
   }
 
   if (newSeason) {
+    // New Season overlay was detected, add to JSON object and make a copy of the file
     const difference = watermarks.filter((x) => !existingWatermarks.includes(x));
     const uri = String(difference[0]);
     const filename = uri.substring(uri.lastIndexOf('/') + 1);
     watermarkToSeason[uri] = newSeason;
-    copyFile(
-      `./data/seasons/watermarks/test/${filename}`,
-      `./data/seasons/watermarks/${newSeason}.png`
-    );
+    copyFile(`${watermarkTestDIR}${filename}`, `${watermarkDIR}${newSeason}.png`);
   }
 
   writeFile('./output/watermark-to-season.json', watermarkToSeason);
