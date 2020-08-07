5552aac0b2cb45af0f70a0c2fece23fd547cf8af
nev <gh@f-m.fm>
2020-08-07 01:54:14 -0700

    include table names so sandbox lookups work

--- src/generate-rich-text-objective.ts
+++ src/generate-rich-text-objective.ts
@@ -1,4 +1,5 @@
 import { getAll, loadLocal } from 'destiny2-manifest/node';
+import stringifyObject from 'stringify-object';
 import { sortObject, writeFile } from './helpers';
 
 loadLocal();
@@ -7,20 +8,31 @@ const perks = getAll('DestinySandboxPerkDefinition');
 
 const iconFinder = /(\[[^\]]+\]|[\uE000-\uF8FF])/g;
 
-const richTexts: Record<string, number> = {};
+type RichTextManifestSourceData = Record<string, [string, number]>;
+const richTexts: RichTextManifestSourceData = {};
 
 objectives.forEach((objective) => {
   const match = objective.progressDescription?.match(iconFinder);
   if (match?.length === 1 && !richTexts[match[0]]) {
-    richTexts[match[0]] = objective.hash;
+    richTexts[match[0]] = ['Objective', objective.hash];
   }
 });
 
 perks.forEach((perk) => {
   const match = perk.displayProperties.description?.match(iconFinder);
   if (match?.length === 1 && !richTexts[match[0]]) {
-    richTexts[match[0]] = perk.hash;
+    richTexts[match[0]] = ['SandboxPerk', perk.hash];
   }
 });
 
-writeFile('./output/objective-richTexts.json', sortObject(richTexts));
+const pretty = `
+type RichTextManifestSourceData = Record<string, [string, number]>;
+
+const richTextManifestSourceData: RichTextManifestSourceData = ${stringifyObject(
+  sortObject(richTexts),
+  {
+    indent: '  ',
+  }
+)};\n\nexport default richTextManifestSourceData;`;
+
+writeFile('./output/objective-richTexts.ts', pretty);
