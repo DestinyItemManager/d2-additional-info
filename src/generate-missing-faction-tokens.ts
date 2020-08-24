a3801c89ef39e974bcae3e276d565ece8b2ea08d
Rob Jones <delphi.actual@gmail.com>
2020-08-24 14:35:08 -0600

    add badVendors too

--- src/generate-missing-faction-tokens.ts
+++ src/generate-missing-faction-tokens.ts
@@ -14,12 +14,18 @@ factions.forEach(({ tokenValues }) => {
 });
 
 const allTokenHashes = uniqAndSortArray(tokenHashes.flat()).map(Number);
-const missingTokenHashes: any = [];
+const missingTokenHashes: number[] = [];
+const badVendors: number[] = [];
 allTokenHashes.forEach((hash) => {
   const item = inventoryItems.find((i) => i.hash === hash);
   if (!item) {
     missingTokenHashes.push(hash);
+    const vendor = factions.find((i) => i.tokenValues?.[hash]);
+    if (vendor) {
+      badVendors.push(vendor.hash);
+    }
   }
 });
 
 writeFile('./output/missing-faction-tokens.json', missingTokenHashes);
+writeFile('./output/bad-vendors.json', badVendors);
