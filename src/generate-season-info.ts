455f4caea6e0ecaf66821e75a3b3fe622afc48b3
Rob Jones <delphi.actual@gmail.com>
2020-09-10 19:44:17 -0600

    tweak for OS agnostic

--- src/generate-season-info.ts
+++ src/generate-season-info.ts
@@ -49,3 +49,4 @@ const lightCapToSeason = Object.values(D2SeasonInfo)
 [...powerCaps].forEach((p, i) => (lightCapToSeason[p] = D2CalculatedSeason + 1 + i));
 writeFile('./output/lightcap-to-season.json', lightCapToSeason);
 copyFile('./data/seasons/d2-season-info.ts', './output/d2-season-info.ts');
+copyFile('./data/legacy-triumphs.json', './output/legacy-triumphs.json');
