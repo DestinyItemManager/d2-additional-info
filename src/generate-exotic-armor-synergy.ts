c9c132a14f1b99a61229f5e10c50e21fff1ced28
Rob Jones <delphi.actual@gmail.com>
2022-09-18 10:09:48 -0600

    exotic armor synergies
    |Exotic Armor|Synergies|
    |------------|---------|
    |Helm of Saint-14|void|
    |Geomag Stabilizers|arc|
    |Mk. 44 Stand Asides|arc,solar,void|
    |Transversive Steps||
    |St0mp-EE5||
    |Lucky Pants||
    |Orpheus Rig|void|
    |Promethium Spur|solar|
    |Doom Fang Pauldron|void|
    |Synthoceps||
    |Nothing Manacles|void|
    |Wings of Sacred Dawn|solar|
    |Gyrfalcon's Hauberk|void|
    |Young Ahamkara's Spine|solar|
    |Secant Filaments|void|
    |Fr0st-EE5||
    |Oathkeeper||
    |Shards of Galanor|solar|
    |Foetracer||
    |Lucky Raspberry|arc|
    |Ophidia Spathe||
    |Star-Eater Scales||
    |Skull of Dire Ahamkara|void|
    |Shinobu's Vow|arc|
    |Apotheosis Veil||
    |Dunemarchers||
    |Gemini Jester||
    |Citan's Ramparts||
    |Hallowfire Heart|solar|
    |The Bombardiers|arc,solar,stasis,void|
    |Knucklehead Radar||
    |Celestial Nighthawk|solar|
    |Hoarfrost-Z|stasis|
    |Heart of Inmost Light||
    |Second Chance|void|
    |The Path of Burning Steps|solar|
    |Icefall Mantle|stasis|
    |Gwisin Vest|void|
    |The Sixth Coyote||
    |Phoenix Cradle|solar|
    |Actium War Rig||
    |Crest of Alpha Lupi||
    |Mask of Bakris|stasis|
    |Rain of Fire||
    |Sealed Ahamkara Grasps||
    |Radiant Dance Machines||
    |Blight Ranger|arc|
    |Point-Contact Cannon Brace|arc|
    |Vesper of Radius||
    |Mechaneer's Tricksleeves||
    |Ashen Wake|solar|
    |Wormgod Caress||
    |Ursa Furiosa|void|
    |Fallen Sunstar|arc|
    |Contraverse Hold|void|
    |Omnioculus|void|
    |Stormdancer's Brace|arc|
    |Starfire Protocol|solar|
    |Renewal Grasps|stasis|
    |The Stag||
    |Assassin's Cowl||
    |Stronghold||
    |Peregrine Greaves|arc,solar,void|
    |Raiju's Harness|arc|
    |Dawn Chorus|solar|
    |Cuirass of the Falling Star|arc|
    |Severance Enclosure||
    |Khepri's Horn||
    |Athrys's Embrace||
    |Antaeus Wards||
    |Verity's Brow||
    |ACD/0 Feedback Fence||
    |One-Eyed Mask||
    |Khepri's Sting||
    |Raiden Flux|arc|
    |The Dragon's Shadow||
    |Graviton Forfeit|void|
    |Necrotic Grip||
    |Eternal Warrior|arc|
    |Felwinter's Helm||
    |Boots of the Assembler||
    |Sanguine Alchemy||
    |Getaway Artist|arc|
    |An Insurmountable Skullfort|arc|
    |Osmiomancy Gloves|stasis|
    |No Backup Plans|void|
    |Claws of Ahamkara||
    |Mantle of Battle Harmony||
    |Loreley Splendor Helm|solar|
    |Crown of Tempests|arc|
    |Eye of Another World||
    |Nezarec's Sin|void|
    |Caliban's Hand|solar|
    |Lion Rampant||
    |Peacekeepers||
    |Wormhusk Crown||
    |Ophidian Aspect||
    |Sunbracers|solar|
    |Karnstein Armlets||
    |Winter's Guile||
    |Armamentarium||
    |Mask of the Quiet One||
    |Astrocyte Verse||
    |Precious Scars||
    |Chromatic Fire||
    |Phoenix Protocol|solar|
    |Lunafaction Boots||
    |Liar's Handshake|arc|

---
+++ src/generate-exotic-armor-synergy.ts
@@ -0,0 +1,67 @@
+import { get, getAll, loadLocal } from '@d2api/manifest-node';
+import { writeFile } from './helpers.js';
+
+loadLocal();
+
+const debug = false;
+const inventoryItems = getAll('DestinyInventoryItemDefinition');
+const exoticSynergy = {} as Record<number, string[]>;
+
+if (debug) {
+  console.log('|Exotic Armor|Synergies|');
+  console.log('|------------|---------|');
+}
+
+inventoryItems.filter(
+  (item) =>
+    item.equippingBlock?.uniqueLabel === 'exotic_armor' &&
+    item.sockets?.socketEntries.find((socket) => {
+      if ([965959289].includes(socket.socketTypeHash)) {
+        let synergy = [];
+        const itemName = item.displayProperties.name;
+        const hash = item.hash;
+        const intrinsicTraitDescription =
+          get(
+            'DestinyInventoryItemDefinition',
+            socket.singleInitialItemHash
+          )?.displayProperties.description.toLowerCase() ?? '';
+        if (
+          /chaos reach|arc staff|skip grenade|arc melee|arc bolt|whirlwind guard|blind(s)?|fists of havoc|thunderclap melee|thundercrash|seismic strike|ionic traces|arc ability|jolt|stormtrance|arc grenade|arc soul/.test(
+            intrinsicTraitDescription
+          ) &&
+          !/sentinel shield/.test(intrinsicTraitDescription) // sentinel shield blinds ...
+        ) {
+          synergy.push('arc');
+        }
+        if (
+          /golden gun|tripmine grenade|blade barrage|scorch(es)?|sunspot|fusion grenades|solar abilities|hammer strike|solar final blows|sol invictus|solar grenades|dawnblade|well of radiance|daybreak/.test(
+            intrinsicTraitDescription
+          )
+        ) {
+          synergy.push('solar');
+        }
+        if (/stasis subclass|duskfield|slows|coldsnap grenades/.test(intrinsicTraitDescription)) {
+          synergy.push('stasis');
+        }
+        if (
+          /spectral blades|moebius quiver|deadfall|suppresses|ward of dawn|sentinel shield|shield throw|void melee energy|shield bash|nova bomb|void-damage|scatter grenade|void grenade|devour|invisible/.test(
+            intrinsicTraitDescription
+          )
+        ) {
+          synergy.push('void');
+        }
+        if (
+          // Naive attempt to catch new exotics on LightFall release
+          /strand/.test(intrinsicTraitDescription)
+        ) {
+          synergy.push('strand');
+        }
+        exoticSynergy[hash] = synergy;
+        if (debug) {
+          console.log(`|${itemName}|${synergy}|`);
+        }
+      }
+    })
+);
+
+writeFile('./output/exotic-synergy.json', exoticSynergy);
