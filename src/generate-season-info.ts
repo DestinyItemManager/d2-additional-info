33c8b22ca556f5ca52defbf7fe21f69327857be4
Rob Jones <delphi.actual@gmail.com>
2022-12-06 14:38:18 -0700

    fixed SEASONS.md

--- src/generate-season-info.ts
+++ src/generate-season-info.ts
@@ -215,10 +215,9 @@ function updateSeasonsMD(seasonNumber: number) {
       : D2SeasonInfo[seasonNumber].seasonName.replace('Season of ', '')
   ).padEnd(12);
 
-  if (paddedReleaseDate != ''.padEnd(10)) {
-    return `\n| ${paddedSeasonNumber} | ${paddedReleaseDate} | ${paddedEndDate} | ${paddedDLCName} | ${paddedSeasonName} |`;
-  }
-  return '';
+  return paddedReleaseDate != ''.padEnd(10)
+    ? `\n| ${paddedSeasonNumber} | ${paddedReleaseDate} | ${paddedEndDate} | ${paddedDLCName} | ${paddedSeasonName} |`
+    : '';
 }
 
 function formatDateDDMMMYYYY(dateString: string, dayBefore = false) {
