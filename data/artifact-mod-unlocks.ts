f3dad8f4d35272bdfa3742ffe779a467ccb1b2eb
robojumper <robojumper@gmail.com>
2023-03-17 20:36:59 +0100

    Generate artifact mod unlocks

---
+++ data/artifact-mod-unlocks.ts
@@ -0,0 +1,47 @@
+export const artifactPlugUnlocks: { [artifactModHash: number]: number[] } = {
+  '3555350688': [
+    2283894334, // Solar Weapon Surge
+    3675553168, // Solar Holster
+    56663992, // Solar Scavenger
+    1086997255, // Solar Siphon
+    331268185, // Solar Targeting
+    1019574576, // Unflinching Solar Aim
+    411014648, // Solar Reserves
+    634608391, // Solar Loader
+    531665167, // Solar Dexterity
+  ],
+  '3555350690': [
+    3914973263, // Void Weapon Surge
+    2634786903, // Void Holster
+    2815817957, // Void Scavenger
+    1210012576, // Void Siphon
+    2888195476, // Void Targeting
+    3887037435, // Unflinching Void Aim
+    2413278875, // Void Reserves
+    3224649746, // Void Loader
+    1017385934, // Void Dexterity
+  ],
+  '3555350691': [
+    1501094193, // Strand Weapon Surge
+    2805854721, // Strand Holster
+    1305848463, // Strand Scavenger
+    3279257734, // Strand Siphon
+    3013778406, // Strand Targeting
+    3598972737, // Unflinching Strand Aim
+    2303417969, // Strand Reserves
+    95934356, // Strand Loader
+    3979300428, // Strand Dexterity
+  ],
+  '3555350692': [
+    2831374162, // Solar/Strand Dual Siphon
+    110793779, // Void/Strand Dual Siphon
+  ],
+  '3555350693': [
+    2136310244, // Ashes to Assets
+    965934024, // Firepower
+    2031584061, // Momentum Transfer
+    3245543337, // Bolstering Detonation
+    1924584408, // Grenade Kickstart
+    1153260021, // Impact Induction
+  ],
+};
