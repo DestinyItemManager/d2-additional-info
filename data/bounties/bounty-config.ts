865c9c02da9222ae84f2bd1418f4f55e8b6ca105
Rob Jones <delphi.actual@gmail.com>
2024-05-22 08:25:18 -0600

    Archer's Line is not Arc damage
    thx @justrealmilk

--- data/bounties/bounty-config.ts
+++ data/bounties/bounty-config.ts
@@ -207,7 +207,7 @@ export const matchTable: {
     assign: {
       DamageType: [DamageHash.Arc],
     },
-    desc: ['Arc'],
+    desc: [/Arc(!her's)/],
     obj: ['[Arc]', 'Arc damage'],
   },
   {
