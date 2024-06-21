6fc990c62d380bff71b823efc099bf729779409f
Rob Jones <delphi.actual@gmail.com>
2024-06-20 18:30:31 -0600

    add special sidearm, remove engram rewards...

--- src/generate-extended-ich.ts
+++ src/generate-extended-ich.ts
@@ -6,16 +6,26 @@ const inventoryItems = getAllDefs('InventoryItem');
 
 const ffGrenadeLaunchers = inventoryItems.filter(
   (item) =>
-    item.itemCategoryHashes?.includes(ItemCategoryHashes.GrenadeLaunchers) &&
+    item.itemCategoryHashes?.includes(ItemCategoryHashes.Weapon) &&
+    item.itemCategoryHashes.includes(ItemCategoryHashes.GrenadeLaunchers) &&
     !item.itemCategoryHashes.includes(ItemCategoryHashes.PowerWeapon) &&
     !item.itemCategoryHashes.includes(ItemCategoryHashes.Dummies),
 );
 
 const slugShotguns = inventoryItems.filter(
   (item) =>
-    item.itemCategoryHashes?.includes(ItemCategoryHashes.Shotgun) &&
+    item.itemCategoryHashes?.includes(ItemCategoryHashes.Weapon) &&
+    item.itemCategoryHashes.includes(ItemCategoryHashes.Shotgun) &&
     !item.itemCategoryHashes.includes(ItemCategoryHashes.Dummies) &&
     item.sockets?.socketEntries[0].singleInitialItemHash === 918679156, // Slug Precision Frame
+);
+
+const specialSidearms = inventoryItems.filter(
+  (item) =>
+    item.itemCategoryHashes?.includes(ItemCategoryHashes.Weapon) &&
+    item.itemCategoryHashes.includes(ItemCategoryHashes.Sidearm) &&
+    !item.itemCategoryHashes.includes(ItemCategoryHashes.Dummies) &&
+    item.equippingBlock?.ammoType === 2, // special ammo
 );
 
 const festivalMasks = inventoryItems.filter((item) =>
@@ -30,6 +40,10 @@ ffGrenadeLaunchers.forEach((gl) => {
 
 slugShotguns.forEach((ssg) => {
   extendedICH[ssg.hash] = -ItemCategoryHashes.Shotgun;
+});
+
+specialSidearms.forEach((ssa) => {
+  extendedICH[ssa.hash] = -ItemCategoryHashes.Sidearm;
 });
 
 festivalMasks.forEach((fm) => {
