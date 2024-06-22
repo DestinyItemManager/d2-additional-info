c14ea0631102d471475d619e6f64a99951b61b97
Ben Hollis <ben@benhollis.net>
2024-06-21 23:52:37 -0700

    Trim extended-ich down to just masks

--- src/generate-extended-ich.ts
+++ src/generate-extended-ich.ts
@@ -4,47 +4,11 @@ import { writeFile } from './helpers.js';
 
 const inventoryItems = getAllDefs('InventoryItem');
 
-const realWeaponsOnly = inventoryItems.filter(
-  (item) =>
-    item.itemCategoryHashes?.includes(ItemCategoryHashes.Weapon) &&
-    !item.itemCategoryHashes.includes(ItemCategoryHashes.Dummies),
-);
-
-const ffGrenadeLaunchers = realWeaponsOnly.filter(
-  (item) =>
-    item.itemCategoryHashes?.includes(ItemCategoryHashes.GrenadeLaunchers) &&
-    !item.itemCategoryHashes.includes(ItemCategoryHashes.PowerWeapon),
-);
-
-const slugShotguns = realWeaponsOnly.filter(
-  (item) =>
-    item.itemCategoryHashes?.includes(ItemCategoryHashes.Shotgun) &&
-    item.sockets?.socketEntries[0].singleInitialItemHash === 918679156, // Slug Precision Frame
-);
-
-const specialSidearms = realWeaponsOnly.filter(
-  (item) =>
-    item.itemCategoryHashes?.includes(ItemCategoryHashes.Sidearm) &&
-    item.equippingBlock?.ammoType === 2, // special ammo
-);
-
 const festivalMasks = inventoryItems.filter((item) =>
   item.itemTypeDisplayName?.includes('Festival Mask'),
 );
 
 const extendedICH: Record<number, number> = {};
-
-ffGrenadeLaunchers.forEach((gl) => {
-  extendedICH[gl.hash] = -ItemCategoryHashes.GrenadeLaunchers;
-});
-
-slugShotguns.forEach((ssg) => {
-  extendedICH[ssg.hash] = -ItemCategoryHashes.Shotgun;
-});
-
-specialSidearms.forEach((ssa) => {
-  extendedICH[ssa.hash] = -ItemCategoryHashes.Sidearm;
-});
 
 festivalMasks.forEach((fm) => {
   extendedICH[fm.hash] = ItemCategoryHashes.Mask;
