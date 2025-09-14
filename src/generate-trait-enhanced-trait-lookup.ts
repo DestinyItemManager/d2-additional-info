ce21f12004eec3a88d0ccc56b0430e3c65fc35ed
Ben Hollis <ben@benhollis.net>
2025-09-14 14:13:21 -0700

    Do it directly from plugsets?

--- src/generate-trait-enhanced-trait-lookup.ts
+++ src/generate-trait-enhanced-trait-lookup.ts
@@ -4,8 +4,16 @@
 import { getAllDefs, getDef } from '@d2api/manifest-node';
 import { DestinyInventoryItemDefinition, TierType } from 'bungie-api-ts/destiny2';
 import { writeFile } from './helpers.js';
+import { warnLog } from './log.js';
+
+const TAG = 'ENHANCED-TRAIT';
+const DEBUG = false;
 
 const traitToEnhancedTraitTable: Record<number, number> = {};
+
+// Also retain which enhanced perks correspond to which base perks because DIM
+// requires the mapping to be invertible and we should correctly prioritize mappings
+const enhancedTraitToTraitTable: Record<number, number> = {};
 
 const matchTraits = (plugs: DestinyInventoryItemDefinition[]) => {
   const traits = plugs.filter((p) => p.displayProperties.name);
@@ -16,36 +24,33 @@ const matchTraits = (plugs: DestinyInventoryItemDefinition[]) => {
     const enhancedTrait = enhancedTraits.find(
       (et) =>
         et.displayProperties.name == bt.displayProperties.name ||
-        (et.displayProperties.name == `${bt.displayProperties.name} Enhanced` &&
-          bt.perks.every((ep) => !et.perks?.every((bp) => bp.perkHash === ep.perkHash))),
+        et.displayProperties.name == `${bt.displayProperties.name} Enhanced`,
     );
 
     if (enhancedTrait) {
-      traitToEnhancedTraitTable[bt.hash] = enhancedTrait.hash;
+      const existingBase = enhancedTraitToTraitTable[enhancedTrait.hash];
+      if (existingBase) {
+        if (existingBase !== bt.hash && DEBUG) {
+          warnLog(
+            TAG,
+            `ignoring base ${bt.displayProperties.name} (${bt.hash}) -> enhanced ${enhancedTrait.displayProperties.name} (${enhancedTrait.hash}) because other mapping (${existingBase}) was previously found`,
+          );
+        }
+      } else {
+        traitToEnhancedTraitTable[bt.hash] = enhancedTrait.hash;
+        enhancedTraitToTraitTable[enhancedTrait.hash] = bt.hash;
+      }
     }
   });
 };
 
-const inventoryItems = getAllDefs('InventoryItem');
-
-// Prefer corresponding traits that come directly from the recipes, as they're certainly correct
-const craftingRecipes = inventoryItems.filter((i) => i.crafting);
-for (const recipe of craftingRecipes) {
-  for (const socket of recipe.sockets!.socketEntries) {
-    if (socket.reusablePlugSetHash) {
-      const plugSet = getDef('PlugSet', socket.reusablePlugSetHash);
-      if (plugSet) {
-        const plugs: DestinyInventoryItemDefinition[] = plugSet.reusablePlugItems
-          .map((i) => getDef('InventoryItem', i.plugItemHash))
-          .filter((p) => p)
-          .map((p) => p!);
-        matchTraits(plugs);
-      }
-    }
-  }
+const plugSets = getAllDefs('PlugSet');
+for (const plugSet of plugSets) {
+  const plugs: DestinyInventoryItemDefinition[] = plugSet.reusablePlugItems
+    .map((i) => getDef('InventoryItem', i.plugItemHash))
+    .filter((p) => p)
+    .map((p) => p!);
+  matchTraits(plugs);
 }
-
-const allTraits = inventoryItems.filter((i) => i.plug?.plugCategoryHash !== undefined);
-matchTraits(allTraits);
 
 writeFile('./output/trait-to-enhanced-trait.json', traitToEnhancedTraitTable);
