33121dcec1de244f49bb167f392a76ee5ece1a1f
Ben Hollis <ben@benhollis.net>
2025-09-14 13:51:16 -0700

    Allow dupes?

--- src/generate-trait-enhanced-trait-lookup.ts
+++ src/generate-trait-enhanced-trait-lookup.ts
@@ -3,18 +3,9 @@
  */
 import { getAllDefs, getDef } from '@d2api/manifest-node';
 import { DestinyInventoryItemDefinition, TierType } from 'bungie-api-ts/destiny2';
-import { PlugCategoryHashes } from '../data/generated-enums.js';
 import { writeFile } from './helpers.js';
-import { warnLog } from './log.js';
-
-const TAG = 'ENHANCED-TRAIT';
-const DEBUG = false;
 
 const traitToEnhancedTraitTable: Record<number, number> = {};
-
-// Also retain which enhanced perks correspond to which base perks because DIM
-// requires the mapping to be invertible and we should correctly prioritize mappings
-const enhancedTraitToTraitTable: Record<number, number> = {};
 
 const matchTraits = (plugs: DestinyInventoryItemDefinition[]) => {
   const traits = plugs.filter((p) => p.displayProperties.name);
@@ -25,22 +16,12 @@ const matchTraits = (plugs: DestinyInventoryItemDefinition[]) => {
     const enhancedTrait = enhancedTraits.find(
       (et) =>
         et.displayProperties.name == bt.displayProperties.name ||
-        et.displayProperties.name == `${bt.displayProperties.name} Enhanced`,
+        (et.displayProperties.name == `${bt.displayProperties.name} Enhanced` &&
+          bt.perks.every((ep) => !et.perks?.every((bp) => bp.perkHash === ep.perkHash))),
     );
 
     if (enhancedTrait) {
-      const existingBase = enhancedTraitToTraitTable[enhancedTrait.hash];
-      if (existingBase) {
-        if (existingBase !== bt.hash && DEBUG) {
-          warnLog(
-            TAG,
-            `ignoring base ${bt.displayProperties.name} (${bt.hash}) -> enhanced ${enhancedTrait.displayProperties.name} (${enhancedTrait.hash}) because other mapping (${existingBase}) was previously found`,
-          );
-        }
-      } else {
-        traitToEnhancedTraitTable[bt.hash] = enhancedTrait.hash;
-        enhancedTraitToTraitTable[enhancedTrait.hash] = bt.hash;
-      }
+      traitToEnhancedTraitTable[bt.hash] = enhancedTrait.hash;
     }
   });
 };
